2935a228e057e33269b9c130a7a28d3b0e90e590
Marildo <marildocesar@hotmail.com>
2021-04-28 17:40:55 -0300

    Implemented clients request

--- front-end/src/views/Client.vue
+++ front-end/src/views/Client.vue
@@ -2,25 +2,38 @@
   <div>
     <h1>Clientes</h1>
     <button @click="loadClients()">Load</button>
+
+    {{ state.clients }}
   </div>
 </template>
 
 <script>
-import { onMounted } from 'vue'
+import { onMounted, reactive } from 'vue'
+import axios from 'axios'
+
 export default {
   name: 'Client',
   setup () {
     onMounted(() => {
-      console.log('mountend')
+      loadClients()
+    })
+
+    const state = reactive({
+      clients: []
     })
 
     function loadClients () {
       console.log('clicked')
-      return 1
+      axios
+        .get('http://127.0.0.1:4000/client')
+        .then(response => response.data)
+        .then(data => (state.clients = data))
+        .catch(erros => console.log(erros))
     }
 
     return {
-      loadClients
+      loadClients,
+      state
     }
   }
 }
