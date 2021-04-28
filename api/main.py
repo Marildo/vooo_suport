e63444219dbd095de6fd47a7bc70fadbbe4347b5
Marildo <marildocesar@hotmail.com>
2021-04-28 17:35:31 -0300

    Added cors

--- api/main.py
+++ api/main.py
@@ -1,11 +1,22 @@
 from datetime import datetime
 from flask import Flask, jsonify, make_response
+from flask_cors import CORS
 from model.marshmallowConfig import init_marshmallow
 from settings.Settings import Settings
 from routers.clientRouter import client_router
 
 
+
 app = Flask('Api-vooo-suport')
+app.config['CORS_HEADERS'] = 'application/json'
+
+cors = CORS(app,
+            resources={
+                r"*": {
+                    "origins": "*"
+                }
+            })
+            
 app.register_blueprint(client_router)
 
 
